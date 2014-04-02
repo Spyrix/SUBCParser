3be3275ad3237884e3e72584b443151b0d99a8c6
William Frazee <frazeew@gmail.com>
2014-04-01 20:46:20 -0400

    Just small changes

--- Scan.java
+++ Scan.java
@@ -3,16 +3,18 @@ import java.util.*;
 public class Scan {
 	private File source;
 	private Scanner scanner;
-	private ArrayList tokenList;
 	public Scan(File source){
 		if(source.exists())
 			this.source = source;
+	}
+	public Scan(){
+		
 	}
 	//scan just one token at a time
 	//SCAN JUST ONE TOKEN AT A TIME?
 	public Token scanFile(){
 		//This method scans the file and partions everything into tokens. First it will check the next token for keywords
-		
+			
 		//case Identifier
 	}
 
--- p1.java
+++ p1.java
@@ -1,6 +1,16 @@
-Public class p1 {
-	public static void main(String[] args){
+import java.util.*;
 
+Public class p1 {
+	private ArrayList<Token> tokenList;
+	Scan scan;
+	private File source;
+	private Scanner scanner;
+
+	public static void main(String[] args){
+		//maybe we should start scanning the file here
+		
+		scan = new Scan();
+		int tokenCounter = 0;
 	}
 
 	public static error(){
